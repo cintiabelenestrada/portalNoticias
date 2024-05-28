bdb0dfc796f77e82389e86f0ab5da09469465930
CintiaBelenEstrada <cintiabelenestrada@gmail.com>
2024-05-28 00:32:41 -0300

    routing component, agregado de providerHttpCLient, limpieza de html principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PortalNoticiasComponent } from './components/portal-noticias/portal-noticias.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PortalNoticiasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { PortalNoticiasComponent } from './components/portal-noticias/portal-noticias.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'noticias',
+        component: PortalNoticiasComponent,
+        title: 'Últimas noticias',
+      },
+];
