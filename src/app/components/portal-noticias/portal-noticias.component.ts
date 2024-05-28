4fd23c50da44f7cb23a9817e43e4f97332789520
CintiaBelenEstrada <cintiabelenestrada@gmail.com>
2024-05-28 11:09:28 -0300

    se consumio la api de noticias, se muestra por consolelog de dos formas, se crearon interfaces

--- src/app/components/portal-noticias/portal-noticias.component.ts
+++ src/app/components/portal-noticias/portal-noticias.component.ts
@@ -1,24 +1,46 @@
 import { Component } from '@angular/core';
 import { PortalNoticiasService } from '../../services/portal-noticias.service';
+import { NoticiaOriginal, Noticias } from '../../models/portal-noticias';
 
 @Component({
   selector: 'app-portal-noticias',
   standalone: true,
   imports: [],
   templateUrl: './portal-noticias.component.html',
-  styleUrl: './portal-noticias.component.css'
+  styleUrl: './portal-noticias.component.css',
 })
 export class PortalNoticiasComponent {
-  constructor(private portalNoticiasService: PortalNoticiasService){
-  }
-  mostrarNoticias(){
+  noticias: Noticias[] = [];
+  noticias2: Noticias[] = [];
+  constructor(private portalNoticiasService: PortalNoticiasService) {}
+  mostrarNoticias() {
     this.portalNoticiasService.getObtenerNoticias().subscribe(
-      (data:any) => {
-      console.log(data);
+      (data: any) => {
+        // console.log("data: ", JSON.stringify(data));
+
+        // 2 FORMAS DE ITERAR LOS ARRAY DE NOTICIAS:
+        // 1ra forma me trae todos los elementos del array, y en el html solo muestro lo que necesito y me evito de hacer interface
+        for (const item of data.categories) {
+          this.noticias.push(item);
+        }
+
+        // 2da Forma va limpiando el array y solo me trae lo que le solicito, title y description
+        this.noticias2 = data.categories.map((item: NoticiaOriginal) => {
+          return {
+            seo: {
+              title: item.seo.title,
+              description: item.seo.description,
+            },
+          };
+        });
+
+        console.log('data.categories: ', JSON.stringify(data.categories));
+        console.log('noticias: ', JSON.stringify(this.noticias));
+        console.log('noticias2: ', JSON.stringify(this.noticias2));
       },
       (error:any) => {
         console.log(error);
       }
-    )
-  };
+    );
+  }
 }
--- src/app/models/portal-noticias.ts
+++ src/app/models/portal-noticias.ts
@@ -1,2 +1,55 @@
-export class PortalNoticias {
+// export class PortalNoticias {
+//   title: string;
+
+//   constructor(title: string) {
+//     this.title = title;
+//   }
+// }
+
+export interface Seo {
+  title: string;
+  description: string;
+}
+
+export interface Noticias {
+  seo: Seo;
+}
+
+export interface CreatedBy {
+  id: string;
+  name: string;
+}
+
+export interface Seo {
+  slug: string;
+  index: boolean;
+  title: string;
+  follow: boolean;
+  keywords: string[];
+  description: string;
+  redirect_type: string | null;
+}
+
+export interface NoticiaOriginal {
+  id: string;
+  pathname: string;
+  description: string | null;
+  active: boolean;
+  listPosition: number;
+  title: string;
+  initialTitle: string;
+  subs: any[];
+  media: any[];
+  createdAt: string;
+  createdBy: CreatedBy;
+  seo: Seo;
+  updatedAt: string;
+  parentId: string | null;
+}
+
+export interface NoticiaTransformada {
+  seo: {
+    title: string;
+    description: string;
+  };
 }
