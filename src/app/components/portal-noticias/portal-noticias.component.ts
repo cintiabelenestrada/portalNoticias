758741ed924efa6eee1dd1800ca40d3566c1b437
CintiaBelenEstrada <cintiabelenestrada@gmail.com>
2024-05-28 01:31:19 -0300

    se levanto correctamente la API noticias

--- src/app/components/portal-noticias/portal-noticias.component.ts
+++ src/app/components/portal-noticias/portal-noticias.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PortalNoticiasService } from '../../services/portal-noticias.service';
 
 @Component({
   selector: 'app-portal-noticias',
@@ -8,5 +9,16 @@ import { Component } from '@angular/core';
   styleUrl: './portal-noticias.component.css'
 })
 export class PortalNoticiasComponent {
-
+  constructor(private portalNoticiasService: PortalNoticiasService){
+  }
+  mostrarNoticias(){
+    this.portalNoticiasService.getObtenerNoticias().subscribe(
+      (data:any) => {
+      console.log(data);
+      },
+      (error:any) => {
+        console.log(error);
+      }
+    )
+  };
 }
